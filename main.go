43ccc7187df6b759ce108f38594e5602227ee6da
tgrangeo <thomas.grangeon9@gmail.com>
2024-06-28 15:46:07 +0200

    checkpoint: multipage + css stylesheet

--- main.go
+++ main.go
@@ -4,45 +4,29 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"html/template"
-	"io"
+
+	// "io"
+	"log"
 	"net/http"
 
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/html"
 	"github.com/gomarkdown/markdown/parser"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
-
-type Templates struct {
-	templates *template.Template
-}
-
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func newTemplate() *Templates {
-	return &Templates{
-		templates: template.Must(template.ParseGlob("views/index.html")),
-	}
-}
 
 type GitHubFileContent struct {
 	Content string `json:"content"`
 }
 
-func fetchGitHubReadme(username string) (string, error) {
-	url := "https://api.github.com/repos/" + username + "/" + username + "/readme"
+func fetchRepoReadme(username string,repo string) (string, error) {
+	url := "https://api.github.com/repos/" + username + "/" + repo + "/contents" + "/README.md"
+	log.Println(url)
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return "", echo.NewHTTPError(resp.StatusCode, "Failed to fetch README file")
-	}
 
 	var fileContent GitHubFileContent
 	if err := json.NewDecoder(resp.Body).Decode(&fileContent); err != nil {
@@ -65,22 +49,86 @@ func fetchGitHubReadme(username string) (string, error) {
 	return string(md), nil
 }
 
+func fetchUserReadme(username string) (string, error) {
+	url := "https://api.github.com/repos/" + username + "/" + username + "/readme"
+	log.Println(url)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var fileContent GitHubFileContent
+	if err := json.NewDecoder(resp.Body).Decode(&fileContent); err != nil {
+		return "", err
+	}
+
+	decodedContent, err := base64.StdEncoding.DecodeString(fileContent.Content)
+	if err != nil {
+		return "", err
+	}
+
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	mdParser := parser.NewWithExtensions(extensions)
+
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	renderer := html.NewRenderer(html.RendererOptions{Flags: htmlFlags})
+
+	md := markdown.ToHTML(decodedContent, mdParser, renderer)
+
+	return string(md), nil
+}
+
+var tmpl *template.Template
+
+func ShowHomePage(w http.ResponseWriter, r *http.Request) {
+	// readme, _ := fetchUserReadme("tgrangeo")
+	readme, err := fetchUserReadme("tgrangeo")
+	//TODO: error handling here
+	if err != nil {
+		log.Fatalln(err)
+		// return tmpl.HTML(http.StatusInternalServerError, fmt.Sprintf("Error fetching README: %v", err))
+	}
+	data := map[string]interface{}{
+		"Readme": template.HTML(readme),
+	}
+
+	tmpl.ExecuteTemplate(w, "index.html", data)
+}
+
+func ShowProjectPage(w http.ResponseWriter, r *http.Request) {
+	readme, err := fetchRepoReadme("tgrangeo", "meteor")
+	log.Println(readme)
+	//TODO: error handling here
+	if err != nil {
+		log.Fatalln(err)
+		// return tmpl.HTML(http.StatusInternalServerError, fmt.Sprintf("Error fetching README: %v", err))
+	}
+	data := map[string]interface{}{
+		"Readme": template.HTML(readme),
+	}
+
+	tmpl.ExecuteTemplate(w, "index.html", data)
+}
+
+func init() {
+	if tmpl == nil {
+		if tmpl == nil {
+			tmpl = template.Must(tmpl.ParseGlob("views/*.html"))
+			template.Must(tmpl.ParseGlob("views/*.html"))
+		}
+	}
+}
+
 func main() {
-	e := echo.New()
-	e.Use(middleware.Logger())
+	http.HandleFunc("/", ShowHomePage)
+	http.HandleFunc("/projects", ShowProjectPage)
 
-	e.Renderer = newTemplate()
 
-	e.GET("/", func(c echo.Context) error {
-		readme, err := fetchGitHubReadme("tgrangeo")
-		if err != nil {
-			return c.HTML(http.StatusInternalServerError, "Error fetching README")
-		}
-		data := map[string]interface{}{
-			"Readme": template.HTML(readme), // Ensure the HTML is not escaped
-		}
-		return c.Render(http.StatusOK, "index", data)
-	})
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	e.Logger.Fatal(e.Start(":42069"))
+	log.Println("🚀 Starting up on port 3000")
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
